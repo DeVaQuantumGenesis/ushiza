8004e7062dd3d75d42bc790bef4d4a70d3b57301
DeVa Computer <devacomp5656@gmail.com>
2025-09-03 21:01:13 +0900

    First commit

--- app/src/main/java/com/example/ushiza/views/MainActivity.kt
+++ app/src/main/java/com/example/ushiza/views/MainActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
