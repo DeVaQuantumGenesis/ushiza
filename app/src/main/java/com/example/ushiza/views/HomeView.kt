04659fc81ac9966ef7643804a67dc0224e710c4f
DeVa Computer <devacomp5656@gmail.com>
2025-09-03 21:29:59 +0900

    Updated

--- app/src/main/java/com/example/ushiza/views/HomeView.kt
+++ app/src/main/java/com/example/ushiza/views/HomeView.kt
@@ -1,12 +1,16 @@
 package com.example.ushiza.views
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -16,7 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
 
 
 class HoneViewActivity: ComponentActivity(){
@@ -25,16 +29,13 @@ class HoneViewActivity: ComponentActivity(){
         setContent {
             Surface {
                 HomeAppBar()
+                FABExample()
             }
         }
     }
 }
 
-@Composable
-fun TitleCard(name: PaddingValues) {
-    Text(text = "Welcome")
-}
-
+//アプリバーのコード
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeAppBar() {
@@ -58,8 +59,45 @@ fun HomeAppBar() {
                 }
             )
         },
-    ) { innerPadding ->
-        TitleCard(innerPadding)
+    ) {
+        //innerPaddingにスクロール可能なビューなどを適用する場合はmodifier: Modifier = Modifierが必要
+        innerPadding ->
+        ScrollViewExample(modifier = Modifier.padding( paddingValues = innerPadding))
     }
 
+}
+
+//スクロール可能なビューのコード
+@Composable
+fun ScrollViewExample(modifier: Modifier = Modifier) {
+    //スクロール状態を監視する関数
+    val scrollState = rememberScrollState()
+
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
+        //スクロール可能なビューに配置したい内容
+        Text(text = "This is the test app")
+    }
+}
+
+//FAB(Floating Action Button)に関するコード
+@Composable
+fun FABExample() {
+   Scaffold(
+       floatingActionButton = {
+           FloatingActionButton(
+               //タップ時のアクションを定義
+               onClick = {}
+           ) {
+               //外観のアイコンを定義
+               Icon(
+                   imageVector = Icons.Default.Add,
+                   contentDescription = "追加"
+               )
+           }
+       }
+   ) {
+       //重ねるビューを定義
+       contentPadding ->
+       ScrollViewExample(modifier = Modifier.padding( paddingValues = contentPadding))
+   }
 }
